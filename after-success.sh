0ef45cda315690ddbb89f90c69462aa292569660
spetz <spetz90@gmail.com>
2017-03-03 10:46:55 +0100

    Fixed myget config

--- after-success.sh
+++ after-success.sh
@@ -1,3 +1,11 @@
-#!/usr/bin/env bash
-echo Triggering Docker Hub registry build using branch $TRAVIS_BRANCH
-curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "'"$TRAVIS_BRANCH"'"}' -X POST $DOCKER_HUB_TRIGGER_URL
+#!/bin/bash
+case "$TRAVIS_BRANCH" in
+  "master")
+    echo Triggering MyGet package build using branch $TRAVIS_BRANCH
+    curl -X POST -d '{}' "$MYGET_TRIGGER_BUILD_PACKAGE_URL"
+    ;;
+  "develop")
+    echo Triggering MyGet package build using branch $TRAVIS_BRANCH
+    curl -X POST -d '{}' "$MYGET_TRIGGER_BUILD_PACKAGE_DEV_URL"
+    ;;    
+esac
