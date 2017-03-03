92a6b5410b06934b4bc934d61ffeb5255288899d
spetz <spetz90@gmail.com>
2017-03-03 10:30:12 +0100

    Build MyGet

---
+++ build.sh
@@ -0,0 +1,2 @@
+dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively/api/v3/index.json" --no-cache
+dotnet pack "Collectively.Services.Storage.Models" -o .
