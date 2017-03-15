d18a3c33f43da9324dfa071e7f51a2e4f27a6f4f
spetz <spetz90@gmail.com>
2017-03-15 14:46:53 +0100

    Updated myget build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively%build-env%/api/v3/index.json" --no-cache
+dotnet restore Collectively.Services.Storage.Models --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/collectively%build-env%/api/v3/index.json" --no-cache
 dotnet pack "Collectively.Services.Storage.Models" -o .
