6f707731d0446c2b20a477181c69e991f259313f
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-04-10 10:17:36 +0200

    service clients module

--- Collectively.Services.Storage/Framework/IoC/ServiceClientsModule.cs
+++ Collectively.Services.Storage/Framework/IoC/ServiceClientsModule.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Linq;
 using Autofac;
-using Collectively.Common.Security;
 using Collectively.Common.ServiceClients;
 using Collectively.Services.Storage.ServiceClients;
 
 namespace Collectively.Services.Storage.Framework.IoC
 {
-    public class ServiceClientsModule : Module
+    public class ServiceClientsModule : ServiceClientsModuleBase
     {
         protected override void Load(ContainerBuilder builder)
         {
@@ -15,21 +12,6 @@ namespace Collectively.Services.Storage.Framework.IoC
             RegisterService<RemarkServiceClient, IRemarkServiceClient>(builder, "remarks");
             RegisterService<UserServiceClient, IUserServiceClient>(builder, "users");
             RegisterService<StatisticsServiceClient, IStatisticsServiceClient>(builder, "statistics");
-        }
-
-        private void RegisterService<TService, TInterface>(ContainerBuilder builder, string title) where TService : TInterface
-        {
-            builder.Register(x =>
-            {
-                var name = x.Resolve<ServicesSettings>()
-                            .Single(s => s.Title == $"{title}-service")
-                            .Name;
-
-                return (TService)Activator.CreateInstance(typeof(TService), 
-                                new object[]{x.Resolve<IServiceClient>(), name});
-            }) 
-            .As<TInterface>()
-            .SingleInstance();
         }
     }
 }
