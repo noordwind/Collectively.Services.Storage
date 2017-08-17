f948fde39dae476d4078b539e759ce46eabae93e
Piotr Gankiewicz <spetz90@gmail.com>
2017-08-17 21:17:04 +0200

    Added state to user info model

--- src/Collectively.Services.Storage.Models/Users/User.cs
+++ src/Collectively.Services.Storage.Models/Users/User.cs
@@ -8,7 +8,6 @@ namespace Collectively.Services.Storage.Models.Users
         public Guid Id { get; set; }
         public string Email { get; set; }
         public string Provider { get; set; }
-        public string State { get; set; }
         public string ExternalUserId { get; set; }
         public string Culture { get; set; }
         public ISet<Guid> FavoriteRemarks { get; set; }
--- src/Collectively.Services.Storage.Models/Users/UserInfo.cs
+++ src/Collectively.Services.Storage.Models/Users/UserInfo.cs
@@ -7,6 +7,7 @@ namespace Collectively.Services.Storage.Models.Users
         public string UserId { get; set; }
         public string Name { get; set; }
         public string Role { get; set; }
+        public string State { get; set; }
         public string AvatarUrl { get; set; }
         public DateTime CreatedAt { get; set; }
     }
