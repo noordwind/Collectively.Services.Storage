d368db4ca831e52c3679cf0899c6c3c42c90791c
Piotr Gankiewicz <spetz90@gmail.com>
2017-08-20 17:06:44 +0200

    Fixed dotnet publish

--- scripts/dotnet-publish.sh
+++ scripts/dotnet-publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet publish ./src/Collectively.Services.Storage -c Release -o ./bin/Docker
+dotnet publish --no-restore ./src/Collectively.Services.Storage -c Release -o ./bin/Docker
